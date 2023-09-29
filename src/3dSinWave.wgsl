847b37f998311ab55847fea5603d44f30c9a3fba
drumath2237 <2237k.drumath@gmail.com>
2023-09-30 05:28:13 +0900

    :bug: 動くつもりで動かないけどいったんコミット

---
+++ src/3dSinWave.wgsl
@@ -0,0 +1,15 @@
+struct Params {
+  positions: array<f32>
+}
+
+@group(0) @binding(0) var<storage,read_write> positionBuffer : Params;
+
+@compute @workgroup_size(1,1,1)
+fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
+    var particleId = global_id.x * u32(100) + global_id.y;
+    var x = positionBuffer.positions[particleId * u32(3)];
+    var z = positionBuffer.positions[particleId * u32(3) + u32(2)];
+    var distance = sqrt(x * x + z * z);
+
+    positionBuffer.positions[particleId * u32(3) + u32(1)] = sin(distance);
+}
--- src/createScene.ts
+++ src/createScene.ts
@@ -7,8 +7,12 @@ import {
   PointsCloudSystem,
   MeshBuilder,
   CloudPoint,
+  ComputeShader,
+  StorageBuffer,
 } from "@babylonjs/core";
 import { randomNumberBetween } from "./utils";
+
+import sinWaveComputeShader from "./3dSinWave.wgsl?raw";
 
 class Playground {
   public static async CreateSceneAsync(
@@ -22,6 +26,19 @@ class Playground {
     const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
 
+    //=============================================================
+
+    const sinWave = new ComputeShader(
+      "3d sin wave",
+      engine,
+      { computeSource: sinWaveComputeShader },
+      {
+        bindingsMapping: {
+          positionBuffer: { group: 0, binding: 0 },
+        },
+      }
+    );
+
     const PARTICLE_COUNT = 100 * 100;
 
     const positionBuffer = new Float32Array(PARTICLE_COUNT * 3);
@@ -30,6 +47,17 @@ class Playground {
       positionBuffer[i * 3 + 1] = 0;
       positionBuffer[i * 3 + 2] = randomNumberBetween(-1, 1);
     }
+    const positionStorage = new StorageBuffer(
+      engine,
+      positionBuffer.byteLength
+    );
+    positionStorage.update(positionBuffer);
+
+    sinWave.setStorageBuffer("positionBuffer", positionStorage);
+
+    await sinWave.dispatchWhenReady(100, 100);
+    const positions = await positionStorage.read();
+    positionBuffer.set(new Float32Array(positions.buffer));
 
     const pointCloud = new PointsCloudSystem("pointCloud", 3, scene, {
       updatable: true,
